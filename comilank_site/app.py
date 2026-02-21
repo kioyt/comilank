10f168431d50e5e4b76af520129c1bfd5a42c4aa
kioyt <entu606@gmail.com>
2026-02-21 22:50:00 +0500

    Update app.py

--- comilank_site/app.py
+++ comilank_site/app.py
@@ -1,18 +1,37 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, render_template, jsonify
+import requests
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'hdjd94j№jm@K2d'
-app.config['WTF_CSRF_ENABLED'] = True
+
+# ВСТАВЬ СЮДА СВОЙ API-КЛЮЧ (начинается с AIza...)
+YOUTUBE_API_KEY = 'AIzaSy...'  # замени на свой ключ
+VIDEO_ID = 'zYrpEl61uCI'
+
+def get_youtube_viewers():
+    url = f'https://www.googleapis.com/youtube/v3/videos?part=liveStreamingDetails&id={VIDEO_ID}&key={YOUTUBE_API_KEY}'
+    try:
+        resp = requests.get(url)
+        data = resp.json()
+        if 'error' in data:
+            return 0, False
+        items = data.get('items', [])
+        if items:
+            details = items[0].get('liveStreamingDetails', {})
+            if 'concurrentViewers' in details:
+                return int(details['concurrentViewers']), True
+        return 0, False
+    except:
+        return 0, False
 
 @app.route('/')
-def home():
+def index():
     return render_template('index.html')
-@app.after_request
-def add_security_headers(response):
-    response.headers['X-Content-Type-Options'] = 'nosniff'
-    response.headers['X-Frame-Options'] = 'DENY'
-    response.headers['X-XSS-Protection'] = '1; mode=block'
-    return response
+
+@app.route('/youtube-viewers')
+def youtube_viewers():
+    viewers, live = get_youtube_viewers()
+    return jsonify(viewers=viewers, live=live)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
